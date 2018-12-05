2c5bc3dadef3b4b0a3da22a86913e81968504503
saboshi69 <42663794+saboshi69@users.noreply.github.com>
2018-12-06 00:11:25 +0800

    searchBar post req and router get lnglat

--- public/autofilladd.js
+++ public/autofilladd.js
@@ -1,3 +1,4 @@
+
 function searchinit() {
 	let input = document.getElementById('locationName');
 	let autocomplete = new google.maps.places.Autocomplete(input);
@@ -5,3 +6,17 @@ function searchinit() {
 google.maps.event.addDomListener(window, 'load', searchinit);
 
 
+
+async function searchByAutofill (){
+	let input = document.getElementById('locationName');
+	let add = input.value
+	axios.post("/searchGeo", add)
+	console.log(add)
+}
+
+
+
+let searchBar = document.querySelector ('#letssearch')
+searchBar.addEventListener("click", ()=>{
+	searchByAutofill()
+})
--- test2/trueSeed.js
+++ test2/trueSeed.js
@@ -12,12 +12,8 @@ const knex = require('knex')({
 
 (async function exp() {
     let arr = [];
-<<<<<<< HEAD
-    let dir = await fs.readdir(`/Users/chian/Desktop/accel/chianlee/propertyStat/getAddressID/data`)
-=======
     //change it plzzzzzzzzzzzzzzzzzz
     let dir = await fs.readdir(`__dirname/../../getAddressID/data`)
->>>>>>> 4e7c550608d5fb7fd3461b49649b8fd5f0975853
 //     let ntwArr = dir.filter((u) => u.includes("NTW") || u.includes("KLN"))
 //     console.log(ntwArr)
 
@@ -25,16 +21,10 @@ const knex = require('knex')({
     await knex('alladdress').del();
 
      for (let element of dir) {
-<<<<<<< HEAD
-         arr.length = 0;
-        let data = await fs.readFile(`/Users/chian/Desktop/accel/chianlee/propertyStat/getAddressID/data/${element}`, "utf8")
-        //let data = await fs.readFile(`/mnt/c/Users/Matthew/Documents/Github/matthewlee/2ndProj/pullSamuel/getAddressID/data/KLN2018_209.json`, "utf8")
-=======
         arr.length = 0;
          //change it plzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
         let data = await fs.readFile(`__dirname/../../getAddressID/data/${element}`, "utf8")
         //let data = await fs.readFile(`/mnt/c/Users/Matthew/Documents/Github/matthewlee/2ndProj/pullSamuel/getAddressID/temp209.json`, "utf8")
->>>>>>> 4e7c550608d5fb7fd3461b49649b8fd5f0975853
         let parsed = JSON.parse(data);
         console.log(parsed.length);
 
