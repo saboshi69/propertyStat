255737e48a513836429b86c681a7c1519cbfe576
matthew-leee <psynergy.lth@gmail.com>
2018-12-08 00:02:36 +0800

    add latlng as id

--- public/form.js
+++ public/form.js
@@ -1,66 +1,68 @@
-const regions = {
-    "HK": {
-        "Kennedy Town/Sai Yin Pun": "101",
-        "Sheung Wan/Central/Admiralty": "117",
-        "South Horizon": "102",
-        "Cyber-port": "104",
-        "Aberdeen/Ap Lei Chau": "105",
-        "Mid Level West": "106",
-        "Happy Valley/Tai Hang": "110",
-        "Mid Level North Point": "112",
-        "Quarry Bay/Kornhill": "113",
-        "Taikoo Shing": "114",
-        "Shau Kei Wan/Chai Wan": "115",
-        "Heng Fa Chuen": "116"
-    },
-    "KLN": {
-        "Olympic Station": "201",
-        "Kowloon Station": "202",
-        "Mongkok/Yaumatei": "203",
-        "Tsimshatsui/Jordan": "204",
-        "Ho Man Tin/King's Park": "205",
-        "To Kwa Wan": "206",
-        "To Kwa Wan East": "219",
-        "Whampoa/Laguna Verde": "207",
-        "Hung Hom": "220",
-        "Tseung Kwan O": "209",
-        "Mei Foo/Wonderland": "210",
-        "Cheung Sha Wan/Shum Shui Po": "212",
-        "Cheung Sha Wan West": "211",
-        "Yau Yat Tsuen/Shek Kip Mei": "213",
-        "Kowloon Tong/Beacon Hill": "214",
-        "Lam Tin/Yau Tong": "215",
-        "Kowloon Bay/Ngau Chi Wan": "216",
-        "Kwun Tong": "217",
-        "Diamond Hill/Wong Tai Sin": "218"
-    },
-    "NTE": {
-        "Sai Kung": "208",
-        "Tai Wai": "301",
-        "Shatin": "302",
-        "Fotan/Shatin Mid Level/Kau To Shan": "303",
-        "Ma On Shan": "304",
-        "Tai Po Mid Level/Hong Lok Yuen": "306",
-        "Tai Po Market/Tai Wo": "307",
-        "Sheung Shui/Fanling": "308"
-    },
-    "NTW": {
-        "Discovery Bay": "103",
-        "Fairview Park/Palm Spring": "309",
-        "Yuen Long": "401",
-        "Tuen Mun": "402",
-        "Tin Shui Wai": "403",
-        "Tsuen Wan": "404",
-        "Kwan Chung": "405",
-        "Tsing Yi": "406",
-        "Ma Wan/Park Island": "407",
-        "Tung Chung/Islands": "408",
-        "Sham Tseng/Castle Peak Road (Tuen Mun Portion)": "409",
-        "Belvedere Garden/Castle Peak Road (Tsuen Wan Portion)": "410"
-    }
-}
+// const regions = {
+//     "HK": {
+//         "Kennedy Town/Sai Yin Pun": "101",
+//         "Sheung Wan/Central/Admiralty": "117",
+//         "South Horizon": "102",
+//         "Cyber-port": "104",
+//         "Aberdeen/Ap Lei Chau": "105",
+//         "Mid Level West": "106",
+//         "Happy Valley/Tai Hang": "110",
+//         "Mid Level North Point": "112",
+//         "Quarry Bay/Kornhill": "113",
+//         "Taikoo Shing": "114",
+//         "Shau Kei Wan/Chai Wan": "115",
+//         "Heng Fa Chuen": "116"
+//     },
+//     "KLN": {
+//         "Olympic Station": "201",
+//         "Kowloon Station": "202",
+//         "Mongkok/Yaumatei": "203",
+//         "Tsimshatsui/Jordan": "204",
+//         "Ho Man Tin/King's Park": "205",
+//         "To Kwa Wan": "206",
+//         "To Kwa Wan East": "219",
+//         "Whampoa/Laguna Verde": "207",
+//         "Hung Hom": "220",
+//         "Tseung Kwan O": "209",
+//         "Mei Foo/Wonderland": "210",
+//         "Cheung Sha Wan/Shum Shui Po": "212",
+//         "Cheung Sha Wan West": "211",
+//         "Yau Yat Tsuen/Shek Kip Mei": "213",
+//         "Kowloon Tong/Beacon Hill": "214",
+//         "Lam Tin/Yau Tong": "215",
+//         "Kowloon Bay/Ngau Chi Wan": "216",
+//         "Kwun Tong": "217",
+//         "Diamond Hill/Wong Tai Sin": "218"
+//     },
+//     "NTE": {
+//         "Sai Kung": "208",
+//         "Tai Wai": "301",
+//         "Shatin": "302",
+//         "Fotan/Shatin Mid Level/Kau To Shan": "303",
+//         "Ma On Shan": "304",
+//         "Tai Po Mid Level/Hong Lok Yuen": "306",
+//         "Tai Po Market/Tai Wo": "307",
+//         "Sheung Shui/Fanling": "308"
+//     },
+//     "NTW": {
+//         "Discovery Bay": "103",
+//         "Fairview Park/Palm Spring": "309",
+//         "Yuen Long": "401",
+//         "Tuen Mun": "402",
+//         "Tin Shui Wai": "403",
+//         "Tsuen Wan": "404",
+//         "Kwan Chung": "405",
+//         "Tsing Yi": "406",
+//         "Ma Wan/Park Island": "407",
+//         "Tung Chung/Islands": "408",
+//         "Sham Tseng/Castle Peak Road (Tuen Mun Portion)": "409",
+//         "Belvedere Garden/Castle Peak Road (Tsuen Wan Portion)": "410"
+//     }
+// }
 
-const regionsGeo = {
+const _ = require("lodash")
+
+const regions = {
     "HK": {
         "Kennedy Town/Sai Yin Pun": [22.279318, 114.13695],
         "Sheung Wan/Central/Admiralty": [22.281949, 114.158249],
@@ -137,11 +139,13 @@ function makeRegion() {
     const bRegions = Object.keys(regions)
     for (let bRegion of bRegions) {
         let parent = document.querySelector(`#${bRegion}`)
-        let sRegions = Object.keys(regions[`${bRegion}`])
-        for (let sRegion of sRegions) {
+        let pairs = _.toPairs(regions[`${bRegion}`])
+        for (let pair of pairs) {
+            let sRegion = pair[0]
+            let latlng = `${pair[1][0]},${pair[1][1]}`;
             let input = document.createElement("input")
             input.setAttribute("type", "checkbox");
-            input.setAttribute("id", `${sRegion}`)
+            input.setAttribute("id", `${latlng}`)
             input.setAttribute("name", "sRegion")
             input.setAttribute("value", `${sRegion}`)
             input.innerHTML = `${sRegion}`;
@@ -190,13 +194,19 @@ document.querySelector(".button > #good").addEventListener("click", async (e) =>
     }).map((u) => {
         return u.getAttribute("value")
     })
+    let latlng = x.filter((u) => {
+        return u.checked == true
+    }).map((u) => {
+        return u.getAttribute("id")
+    })
 
     let json = await axios.post("/", {
         sRegion: sRegion,
         actualArea: aA,
         price: p,
         actualPrice: aP,
-        date: date
+        date: date,
+        latlng: latlng
     })
     //got data without redirect!
     chartType(chart, json.data, sRegion, date)
