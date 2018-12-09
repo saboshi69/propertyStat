900253b2b7c4c8c016028cd9afd1388d3c1c6539
matthew-leee <psynergy.lth@gmail.com>
2018-12-10 00:42:44 +0800

    just pull

--- dbEnquiry/dbEnquiry.js
+++ dbEnquiry/dbEnquiry.js
@@ -35,18 +35,20 @@ function filterTime(duration, u) {
     }
 }
 
-let dummy = { sRegion: [ 'Ma Wan/Park Island' ],
-  actualArea: [],
-  price: [ '3-6' ],
-  actualPrice: [],
-  date: [ '365' ],
-  latlng: [ '22.350075,114.059207' ] }
+let dummy = {
+    sRegion: ['Ma Wan/Park Island'],
+    actualArea: [],
+    price: ['3-6'],
+    actualPrice: [],
+    date: ['365'],
+    latlng: ['22.350075,114.059207']
+}
 
-  //dbData(dummy).then((data)=>{console.log (data)})
+//dbData(dummy).then((data) => { console.log(data) })
 
 async function dbData(json) {
     let jsonArr = await _.toPairs(json).filter((u) => { return u[1][0] != undefined })
-    let result = await knex.select("sRegion", "actualArea", "price", "actualPrice", "date").from("alladdress")
+    let result = await knex.select("sRegion", "address", "actualArea", "price", "actualPrice", "date", "lat", "lng").from("alladdress")
     for (let col of jsonArr) {
         if (col[0] == "sRegion") {
             result = await result.filter((u) => {
@@ -66,15 +68,55 @@ async function dbData(json) {
                 result = await result.filter((u) => {
                     return u[`${col[0]}`] > Number(arr[0]) && u[`${col[0]}`] < Number(arr[1])
                 })
-            } else if (!col[1][0].includes(",")){
+            } else if (!col[1][0].includes(",")) {
                 let duration = Number(col[1][0])
                 result = await result.filter((u) => {
                     return filterTime(duration, u)
                 })
             }
+
+            //from original dbGeocode.js
+            else if (col[1][0].includes(",")) {
+                let latlng = col[1][0].split(",")
+                let clat = latlng[0];
+                let clng = latlng[1];
+                result = await result
+                    .filter((u) => {
+                        let lat = Number(u.lat);
+                        let lng = Number(u.lng);
+                        let distance = measure(lat, lng, clat, clng)
+                        return distance < 1500
+                    })
+                    .map((u) => {
+                        let address = Object.values(u.address).filter((a) => { return a.length > 0 }).join()
+                        return {
+                            sRegion: u.sRegion,
+                            address: address,
+                            actualArea: u.actualArea,
+                            actualPrice: u.actualPrice,
+                            price: u.price,
+                            date: u.date,
+                            lat: u.lat,
+                            lng: u.lng
+                        }
+                    })
+            }
         }
     }
     return result
+}
+
+function measure(lat1, lng1, lat2, lng2) {  // generally used geo measurement function
+    var R = 6378.137; // Radius of earth in KM
+    var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+    var dLng = lng2 * Math.PI / 180 - lng1 * Math.PI / 180;
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+        Math.sin(dLng / 2) * Math.sin(dLng / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+
+    return d * 1000; // meters
 }
 
 
