ec1116b5c31e627981995c592c0de6788df01830
matthew-leee <psynergy.lth@gmail.com>
2018-12-11 21:58:11 +0800

    paginator works

--- public/listSearchData.js
+++ public/listSearchData.js
@@ -31,48 +31,95 @@ async function listSearchData(data) {
     //end of my filter logic
 
     //below is irrelevant to filtering
-    let parent = document.querySelector("#fkingLong")
-    for (let u of cleanData) {
-        let length = cleanData.length -1;
-        
-        if (data.indexOf(u) > 0) {
-            let div = document.createElement("div");
-            div.setAttribute("class", "searchResult");
 
-            let sr = document.createElement("p");
-            sr.setAttribute("class", "searchContent srSearch")
-            sr.innerHTML = `Region: ${u.sRegion}`
+    let superParent = document.querySelector("#fkingLong")
+    let itemsPerPage = 30;
+    let length = cleanData.length - 1;
+    let page = Math.ceil(length / itemsPerPage);
+    let groupData = chunk(cleanData, itemsPerPage)
+    let btnParent = document.querySelector("#paginator")
+    for (let i = 0; i < page; i++) {
+        //create btn
+        let pag = document.createElement("button");
+        pag.setAttribute("class", "pag");
+        pag.innerHTML = `${i + 1}`;
+        btnParent.appendChild(pag);
 
-            let add = document.createElement("p");
-            add.setAttribute("class", "searchContent addSearch")
-            add.innerHTML = `Address: ${u.address}`
+        //create div container
+        let parent = document.createElement("div");
+        parent.setAttribute("class", `container container${i + 1}`);
+        superParent.appendChild(parent);
 
-            let aA = document.createElement("p");
-            aA.setAttribute("class", "searchContent aASearch")
-            aA.innerHTML = `Actual Area: ${u.actualArea}ft`
+        //put things inside parent
+        for (let u of groupData[i]) {
 
-            let aP = document.createElement("p");
-            aP.setAttribute("class", "searchContent aPSearch")
-            aP.innerHTML = `Price per ft(Actual Area): $${u.actualPrice}`
+            if (data.indexOf(u) > 0) {
+                let div = document.createElement("div");
+                div.setAttribute("class", "searchResult");
 
-            let p = document.createElement("p");
-            p.setAttribute("class", "searchContent pSearch")
-            p.innerHTML = `Price: $${u.price}M`
+                let sr = document.createElement("p");
+                sr.setAttribute("class", "searchContent srSearch")
+                sr.innerHTML = `Region: ${u.sRegion}`
 
-            let bookmark = document.createElement("button");
-            bookmark.setAttribute("id", `${u.id}`)
-            bookmark.innerHTML = `Bookmark Property No. ${u.id}`
+                let add = document.createElement("p");
+                add.setAttribute("class", "searchContent addSearch")
+                add.innerHTML = `Address: ${u.address}`
 
-            div.appendChild(sr);
-            div.appendChild(add);
-            div.appendChild(aA);
-            div.appendChild(aP);
-            div.appendChild(p);
-            div.appendChild(bookmark)
+                let aA = document.createElement("p");
+                aA.setAttribute("class", "searchContent aASearch")
+                aA.innerHTML = `Actual Area: ${u.actualArea}ft`
 
-            parent.appendChild(div)
+                let aP = document.createElement("p");
+                aP.setAttribute("class", "searchContent aPSearch")
+                aP.innerHTML = `Price per ft(Actual Area): $${u.actualPrice}`
+
+                let p = document.createElement("p");
+                p.setAttribute("class", "searchContent pSearch")
+                p.innerHTML = `Price: $${u.price}M`
+
+                let bookmark = document.createElement("button");
+                bookmark.setAttribute("id", `${u.id}`)
+                bookmark.innerHTML = `Bookmark Property No. ${u.id}`
+
+                div.appendChild(sr);
+                div.appendChild(add);
+                div.appendChild(aA);
+                div.appendChild(aP);
+                div.appendChild(p);
+                div.appendChild(bookmark)
+
+                parent.appendChild(div)
+            }
         }
+
+        Array.from(document.querySelectorAll("#fkingLong > div")).map((a)=>{
+            let classname = a.getAttribute("class");
+            let num = classname.slice(19)
+            if (num == 1){
+                a.style.display = "flex";
+            } else {
+                a.style.display = "none"
+            }
+        })
+
+        //paginator logic
+        Array.from(document.querySelectorAll(".pag")).map((u) => {
+            return u.addEventListener("click", (e) => {
+                e.preventDefault();
+                let index = u.innerHTML;
+                Array.from(document.querySelectorAll("#fkingLong > div")).map((v) => {
+                    let classname = v.getAttribute("class");
+                    let num = classname.slice(19)
+                    if (num == index) {
+                        v.style.display = "flex";
+                    } else {
+                        v.style.display = "none";
+                    }
+                })
+            })
+        })
     }
+
 }
 
 function measure(lat1, lng1, lat2, lng2) {  // generally used geo measurement function
@@ -86,4 +133,14 @@ function measure(lat1, lng1, lat2, lng2) {  // generally used geo measurement fu
     var d = R * c;
 
     return d * 1000; // meters
+}
+
+function chunk(array, size) {
+    const chunked_arr = [];
+    let index = 0;
+    while (index < array.length) {
+        chunked_arr.push(array.slice(index, size + index));
+        index += size;
+    }
+    return chunked_arr;
 }
