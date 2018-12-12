61bc836aff208bdbc31b808284bd3d0827e4dd4f
chian88 <leechianyee@gmail.com>
2018-12-12 17:33:47 +0800

    dfads

--- public/main.js
+++ public/main.js
@@ -1,21 +1,38 @@
 
 
-
-document.querySelector("#login").addEventListener("click", (e) => {
-	e.preventDefault();
-	window.location = "/login"
-})
-document.querySelector("#register").addEventListener("click", (e) => {
-	e.preventDefault();
-	window.location = "/register"
-})
+if(document.querySelector("#login")){
+	document.querySelector("#login").addEventListener("click", (e) => {
+		e.preventDefault();
+		window.location = "/login"
+	})
+}
 
 
+if(document.querySelector("#register")) {
+	document.querySelector("#register").addEventListener("click", (e) => {
+		e.preventDefault();
+		window.location = "/register"
+	})
+}
 
 
-document.querySelector("#errRedirect").addEventListener("click", (e)=>{
-	e.preventDefault();
-	window.location = "/login"
-})
 
 
+if (document.querySelector("#errRedirect")) {
+	document.querySelector("#errRedirect").addEventListener("click", (e)=>{
+		e.preventDefault();
+		window.location = "/login"
+	})	
+}
+
+
+
+$("#mapButton").on("click", function(e) {
+	$("#chartArea").hide();
+	$("#mapArea").show();
+});
+
+$("#chartButton").on("click", function(e) {
+	$("#mapArea").hide();
+	$("#chartArea").show();
+});
